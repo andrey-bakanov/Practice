6a00be694805084416945ca55aab5862d006667f
Баканов Андрей <homer_rus@hotmail.com>
2023-02-11 00:32:17 +0300

    merge lists

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,9 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using BenchmarkDotNet.Running;
-using ConsoleApp.Bloom;
+//using BenchmarkDotNet.Running;
+//using ConsoleApp.Bloom;
 
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 
-BloomTest.Test();
+//BloomTest.Test();
 
 //var summary = BenchmarkRunner.Run(typeof(SimpleHash));
+
+
+using ConsoleApp.Tasks;
+
+MergeLists lists = new MergeLists();
+lists.Execute();
